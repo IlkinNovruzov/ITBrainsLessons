986b033e1ea1586cd8742d35a6d56e3d0be51058
IlkinNovruzov <inovruzov2004@gmail.com>
2023-08-05 20:28:00 +0400

    Lesson4 Update

---
+++ Lesson4/Task2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number:");
+            int n=int.Parse(Console.ReadLine());
+            for(int i = 1; i < 10; i++)
+            {
+                for(int j = 1; j <=n; j++)
+                {
+                    Console.WriteLine(i+"x"+j+"="+i*j);
+                }
+                Console.WriteLine("---------");
+            }
+        }
+    }
+}
