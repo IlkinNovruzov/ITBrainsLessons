02385d8cddc35faeaa3807cb01c86b91d346ecd4
IlkinNovruzov <inovruzov2004@gmail.com>
2023-08-06 11:26:23 +0400

    Update

---
+++ Lesson4/Task3/Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator();
+        }
+        static void calculator()
+        {
+            Console.Write("Enter number:");
+            double num1=double.Parse(Console.ReadLine());
+            Console.Write("Enter number:");
+            double num2=double.Parse(Console.ReadLine());
+            Console.WriteLine("1:+\n" +
+                              "2:-\n" +
+                              "3:*\n" +
+                              "4:/\n");
+            Console.Write("Choose operation number:");
+            string operation = Console.ReadLine();
+            switch (operation) {
+                case "1":
+                    Console.WriteLine($"{num1}+{num2}={num1 + num2}");
+                    break;
+                case "2":
+                    Console.WriteLine($"{num1}-{num2}={num1 - num2}");
+                    break;
+                case "3":
+                    Console.WriteLine($"{num1}*{num2}={num1 * num2}");
+                    break;
+                case "4":
+                    Console.WriteLine($"{num1}/{num2}={num1 / num2}");
+                    break;
+                default:
+                    Console.WriteLine("There is not operation.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Lesson4/Task4/Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // library Book Inventory Management
+            string[] bookTitles = { "Book 1", "Book 2", "Book 3", "Book 4", "Book 5" };
+            string[] bookAuthors = { "Author 1", "Author 2", "Author 3", "Author 4", "Author 5" };
+            string[] bookGenres = { "Fiction", "Mystery", "Romance", "Fantasy", "Sci-Fi" };
+            int[] bookCopiesAvailable = { 5, 3, 7, 0, 2 };
+            for (int i = 0;i<5;i++)
+            {
+                Console.WriteLine($"{bookTitles[i]}: {bookAuthors[i]},{bookGenres[i]},{ bookCopiesAvailable[i]}");
+            }
+            Console.WriteLine("enter the title of a book:");
+            string input=Console.ReadLine().ToUpper();
+            int c = 0;
+            for(int i=0;i<bookTitles.Length;i++) { 
+                if (input == bookTitles[i].ToUpper())
+                {
+                    Console.WriteLine($"{bookTitles[i]}: {bookAuthors[i]},{bookGenres[i]},{bookCopiesAvailable[i]}");
+                    c++;
+                }
+            } if (c == 0)
+            {
+                Console.WriteLine("The book is not in the inventory.");
+            }
+                
+        }
+        
+           
+       
+        
+    }
+}
