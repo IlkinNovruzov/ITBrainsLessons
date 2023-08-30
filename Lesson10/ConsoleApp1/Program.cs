b78ac8381eb2651b775008b6b2ffd5e84abdf308
IlkinNovruzov <inovruzov2004@gmail.com>
2023-08-30 20:09:23 +0400

    Update

---
+++ Lesson10/ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MyRegion
+            //string input = "hello";
+            //for (int i = input.Length - 1; i >= 0; i--)
+            //{
+            //    Console.Write(input[i]);
+            //} 
+            #endregion
+            #region poli
+            //while (true)
+            //{
+            //    Console.WriteLine("Enter:");
+            //    string input = Console.ReadLine();
+            //    int c = 0;
+            //    for (int i = 0; i < input.Length; i++)
+            //    {
+            //        if (input[i] == input[input.Length - (i + 1)])
+            //        {
+            //            c++;
+            //        }
+            //    }
+            //    if (c / 2 == input.Length / 2)
+            //    {
+            //        Console.WriteLine("Polindromdur.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Polindrom deyil.");
+            //    }
+            //}
+            #endregion
+            #region Dict
+            //IDictionary<string, string> dict = new Dictionary<string, string>();
+            //Console.Write("How many country do you add:");
+            //int count = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i < count; i++)
+            //{
+            //    Console.WriteLine("Enter country:");
+            //    string country = Console.ReadLine();
+            //    Console.WriteLine("Enter capital:");
+            //    string capital = Console.ReadLine();
+            //    dict.Add(country, capital);
+            //}
+
+            //Console.Write("Enter country:");
+            //string input = Console.ReadLine().ToLower();
+
+            // if (input == "all")
+            //{
+            //    foreach (string item in dict.Values)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}else if(dict.ContainsKey(input))
+            //{
+            //    string n = dict[input];
+            //    Console.WriteLine(n);
+            //}
+            #endregion
+
+        }
+    }
+}
