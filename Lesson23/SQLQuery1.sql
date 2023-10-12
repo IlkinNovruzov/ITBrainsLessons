12b68204c200271025a51c5a766ee75e46728f83
IlkinNovruzov <inovruzov2004@gmail.com>
2023-10-12 17:24:28 +0400

    Update

--- Lesson23/SQLQuery1.sql
+++ Lesson23/SQLQuery1.sql
@@ -1,8 +1,19 @@
 create database Academy
 use Academy
 create table Student(
-grade varchar(50),
+ID int Primary Key,
+grade float,
 fullName varchar(50),
-birthDate datetime,
-groupNo int
+birthDate date,
+groupNo varchar(50)
 )
+drop table Student
+insert into Student values (1,90,'Ehliman Muxtrali','01-01-2005','500.2E')
+select * from Student
+
+update Student
+set fullName='Ehliman Muxtarli'
+where ID=1
+update Student
+set grade=85.5
+where ID=1
